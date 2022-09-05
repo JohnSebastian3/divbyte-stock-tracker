a8dd36aea56dd5e766ea18503e0b70e322c7d09d
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-09-04 21:27:12 -0400

    refactor functions to be more dry

--- public/js/main.js
+++ public/js/main.js
@@ -1,53 +1,61 @@
 
-let totalValue;
-let annualDividend;
+let totalValue = 0;
+let annualDividend = 0;
 
-// Get current annual dividen income
-async function getDividend() {
-  let stockList = document.querySelectorAll('li');
-  annualDividend = 0;
+
+async function getStockData() {
+  let stockList = document.querySelectorAll('.stock');
   for(let i = 0; i < stockList.length; i++) {
     let ticker = stockList[i].children[0].innerText.slice(8);
     let shares = Number(stockList[i].children[1].innerText.slice(7));
+    let avgCost = Number(stockList[i].children[2].innerText.slice(8));
     console.log(ticker);
     console.log(shares);
+    console.log(avgCost);
     try {
-      const response = await fetch(
+      const divResponse = await fetch(
         `https://financialmodelingprep.com/api/v3/historical-price-full/stock_dividend/${ticker}?apikey=ac08be8670bfbfba904e1e17d7596342`);
-      const stockInfo = await response.json();
-      console.log(`Stock info`, stockInfo);
-      annualDividend += shares * stockInfo.historical[0].dividend * 4; // quarterly for now, will add algo to change this soon
-      
+      const historicDividends = await divResponse.json();
+      console.log(`Historic Dividend info`, historicDividends);
+
+      const quoteResponse = await fetch(
+        `https://financialmodelingprep.com/api/v3/quote/${ticker}?apikey=ac08be8670bfbfba904e1e17d7596342`
+      )
+      const stockQuote = await quoteResponse.json();
+      console.log('Current stock quote', stockQuote);
+      const change = stockQuote[0].change;
+      console.log(change);
+      const priceChange =  document.createElement('span')
+      priceChange.innerText = `Change: $${change.toFixed(2)} (${(stockQuote[0].changesPercentage).toFixed(2)}%)`;
+      stockList[i].append(priceChange);
+
+      totalValue += shares * (stockQuote[0].price);
+      const currentPrice = document.createElement('span')
+      currentPrice.innerText = ` Current Price: $${(stockQuote[0].price).toFixed(2)}`;
+      stockList[i].append(currentPrice);
+      const profitLoss = document.createElement('span')
+      profitLoss.innerText = ` Proft/Loss: $${(shares * (stockQuote[0].price - avgCost)).toFixed(2)}`;
+      stockList[i].append(profitLoss);
+
+      annualDividend += shares * historicDividends.historical[0].dividend * 4; // quarterly for now, will add algo to change this soon
+      const dividendYield = ((historicDividends.historical[0].dividend * 4 / stockQuote[0].price) * 100).toFixed(2)
+      console.log(`DIVIDEND YIELD: %${dividendYield}`);
+      const dividend = document.createElement('span')
+      dividend.innerText = ` Dividend: ${dividendYield}%`;
+      stockList[i].append(dividend);
+
+      stockList[i].append(deleteLink);
     } catch(err) {
       console.error(err);
     }
     document.querySelector('.yearlyDividend').innerText = annualDividend.toFixed(2);
+    document.querySelector('.totalValue').innerText = totalValue.toFixed(2);
+    
+
   }
   calculateRealTime();
 }
 
-// Get current price of the ticker
-async function getCurrentPrice() {
-  let stockList = document.querySelectorAll('li');
-  totalValue = 0;
-  for(let i = 0; i < stockList.length; i++) {
-    let ticker = stockList[i].children[0].innerText.slice(8);
-    let shares = Number(stockList[i].children[1].innerText.slice(7));
-    console.log(ticker);
-    console.log(shares);
-    try {
-      const response = await fetch(
-            `https://financialmodelingprep.com/api/v3/quote-short/${ticker}?apikey=ac08be8670bfbfba904e1e17d7596342`
-          )
-      const stockPrice = await response.json();
-      totalValue += shares * (stockPrice[0].price);
-    } catch(err) {
-      console.error(err);
-    }
-  }
-  
-  document.querySelector('.totalValue').innerText = totalValue.toFixed(2);
-}
 
 function calculateRealTime() {
   const dividend = document.querySelector('.yearlyDividend');
@@ -59,17 +67,18 @@ function calculateRealTime() {
   const hourly = daily / 24;
   const minute = hourly / 60;
   const second = minute / 60;
-  console.log('yearly:', yearlyDividend);
-  console.log('monthly:', monthly);
-  console.log('daily:', daily);
-  console.log('hourly:', hourly);
-  console.log('minute:', minute);
-  console.log('second:', second);
+  // console.log('yearly:', yearlyDividend);
+  // console.log('monthly:', monthly);
+  // console.log('daily:', daily);
+  // console.log('hourly:', hourly);
+  // console.log('minute:', minute);
+  // console.log('second:', second);
   setInterval(() => {
     totalValue += second / 2;
     realTime.innerText = totalValue;
   }, 500)
 }
 
-getCurrentPrice();
-getDividend();
+// getCurrentPrice();
+// getDividend();
+getStockData();

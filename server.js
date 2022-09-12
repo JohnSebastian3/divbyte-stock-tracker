89eb2057e31e764b48006969b0d84be0b4b01af9
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-09-11 20:27:51 -0400

    remove /users route from server

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
 app.use('/dashboard', require('./routes/dashboard'));
 
 

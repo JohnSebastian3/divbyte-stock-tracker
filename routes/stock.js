1ce77b2c51f492e56f8a3dda117efeae571850ad
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-10-11 19:47:43 -0400

    Add put and delete routes for comments and add ensureAuth middleware

--- routes/stock.js
+++ routes/stock.js
@@ -4,6 +4,7 @@ const {ensureAuth, ensureGuest} = require('../middleware/auth');
 const stockController = require('../controllers/stock');
 
 router.get('/:ticker', stockController.getStock);
-router.post('/:ticker/addComment', stockController.addComment);
-
+router.post('/:ticker/addComment', ensureAuth, stockController.addComment);
+router.put('/:ticker/likeComment/:commentID', ensureAuth, stockController.likeComment);
+router.delete('/:ticker/deleteComment/:commentID', ensureAuth, stockController.deleteComment);
 module.exports = router;

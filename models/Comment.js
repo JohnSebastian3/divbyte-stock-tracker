6cebbb06bee40e540bcb8b9fb3d11a8d2304534d
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-10-10 21:30:28 -0400

    Add comment model

---
+++ models/Comment.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: Number
+  },
+  ticker: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Comment', CommentSchema);

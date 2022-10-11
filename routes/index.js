13d092db238453051cf69822daa220110f8b9873
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-10-10 21:36:13 -0400

    Add profile/id route to get profile of clicked user

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const {ensureAuth, ensureGuest} = require('../middleware/auth');
 const authController = require('../controllers/auth');
 
 
+
 router.get('/', homeRouter.getHome);
+router.get('/profile/:id', homeRouter.getProfile);
 router.get('/login', ensureGuest, authController.getLogin);
 router.post('/login', authController.postLogin);
 router.post('/register', authController.postRegister);

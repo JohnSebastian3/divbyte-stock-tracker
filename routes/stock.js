eca6867f703fdf8bf26fd69bc0155d00c10cccbc
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-10-10 21:31:25 -0400

    Add ticker comment route

--- routes/stock.js
+++ routes/stock.js
@@ -4,5 +4,6 @@ const {ensureAuth, ensureGuest} = require('../middleware/auth');
 const stockController = require('../controllers/stock');
 
 router.get('/:ticker', stockController.getStock);
+router.post('/:ticker/addComment', stockController.addComment);
 
 module.exports = router;

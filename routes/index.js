90994671456428dc4c8b8d4341a21cfe64555ff5
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-11-17 16:51:28 -0500

    rename homeRouter to homeController

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const homeRouter = require("../controllers/home");
+const homeController = require("../controllers/home");
 const { ensureGuest } = require("../middleware/auth");
 const authController = require("../controllers/auth");
 
-router.get("/", homeRouter.getHome);
+router.get("/", homeController.getHome);
 router.get("/login", ensureGuest, authController.getLogin);
 router.post("/login", authController.postLogin);
 router.post("/register", authController.postRegister);
